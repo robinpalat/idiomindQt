243bcc6b8de3b9ce8c5827945bd4d5a47a9e34c3
Robin Palat <robinpalat@gmail.com>
2026-09-10 20:29:24 -0300

    delete build dir

--- build-android/.qt/idiomindqt_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "idiomindqt")
-set(working_dir "/home/robin/1Devin/Projects/Idiomind/IdiomindQt")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/robin/1Devin/Projects/Idiomind/IdiomindQt/build-android/.qt/idiomindqt_res.txt")
